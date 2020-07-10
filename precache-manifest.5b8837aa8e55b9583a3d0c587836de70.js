c418147b879a0e5dcf230ba57ee7131a4d187f2b
Samarth MS <johnx4321gaming@gmail.com>
2020-07-10 16:03:33 +0530

    Updates

---
+++ precache-manifest.5b8837aa8e55b9583a3d0c587836de70.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a34e944b1514efb09bddfdf15d5c13",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ca6fdb22ebf23c5907cd",
+    "url": "/home/static/css/2.46199841.chunk.css"
+  },
+  {
+    "revision": "8c1c163aa13e5f081a22",
+    "url": "/home/static/css/main.ec034d49.chunk.css"
+  },
+  {
+    "revision": "ca6fdb22ebf23c5907cd",
+    "url": "/home/static/js/2.20aca57d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/home/static/js/2.20aca57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1c163aa13e5f081a22",
+    "url": "/home/static/js/main.7d6532a6.chunk.js"
+  },
+  {
+    "revision": "de9d24d3231e17cbeac4",
+    "url": "/home/static/js/runtime-main.595f2ffb.js"
+  },
+  {
+    "revision": "56ce0b9348aa534934714e616fcfc056",
+    "url": "/home/static/media/banner3.56ce0b93.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/home/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/home/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/home/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/home/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/home/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "338aa1b268eda2b6eecc62850c178f9e",
+    "url": "/home/static/media/iot1.338aa1b2.png"
+  },
+  {
+    "revision": "6f95a1b0c6ac5b98959c71fb140b4298",
+    "url": "/home/static/media/iot2.6f95a1b0.jpg"
+  },
+  {
+    "revision": "1f9388bda157805699318ac2b4516deb",
+    "url": "/home/static/media/prod1.1f9388bd.jpeg"
+  },
+  {
+    "revision": "eed901c5f553c055cfcfe725656708ec",
+    "url": "/home/static/media/prod2.eed901c5.jpeg"
+  },
+  {
+    "revision": "797c6c3b06953c56df6e6605e6a283db",
+    "url": "/home/static/media/prod3.797c6c3b.jpeg"
+  },
+  {
+    "revision": "8bd6e247f3372e78038c72f33d849f2e",
+    "url": "/home/static/media/prod4.8bd6e247.jpeg"
+  },
+  {
+    "revision": "4161a40a583ac84e32c37104917e4cab",
+    "url": "/home/static/media/prod5.4161a40a.jpeg"
+  },
+  {
+    "revision": "1e88231fb6257cf7f06ec38d5898df86",
+    "url": "/home/static/media/prod6.1e88231f.jpeg"
+  },
+  {
+    "revision": "e9700c38aec859c943f3e8b52053cd06",
+    "url": "/home/static/media/prod7.e9700c38.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/home/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/home/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/home/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/home/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7aed738105acdf4bb631c755e2499eed",
+    "url": "/home/static/media/thingsenzlogo.7aed7381.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8985bcc7d72dad47da26ab33db13334.js"
+  "/home/precache-manifest.5b8837aa8e55b9583a3d0c587836de70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
